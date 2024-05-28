2004Scape/Client2

commit c2abe2b40bc32c005a80999a8d506f20b702a835

feat: use DataView for the Packet class

src/js/jagex2/io/Packet.ts
@@ -1,4 +1,4 @@
-import {arraycopy, bigIntModPow, bigIntToBytes, bytesToBigInt} from '../util/JsUtil';
+import {bigIntModPow, bigIntToBytes, bytesToBigInt} from '../util/JsUtil';
 import Isaac from './Isaac';
 import LinkList from '../datastruct/LinkList';
 import Hashable from '../datastruct/Hashable';
@@ -47,7 +47,8 @@ export default class Packet extends Hashable {
     };
 
     // constructor
-    readonly data: Uint8Array;
+    private readonly view: DataView;
+    data: Uint8Array;
     pos: number;
 
     // runtime
@@ -64,15 +65,16 @@ export default class Packet extends Hashable {
         } else {
             this.data = src;
         }
+        this.view = new DataView(this.data.buffer);
         this.pos = 0;
     }
 
     get length(): number {
-        return this.data.length;
+        return this.view.byteLength;
     }
 
     get available(): number {
-        return this.data.length - this.pos;
+        return this.length - this.pos;
     }
 
     static alloc = (type: number): Packet => {
@@ -103,65 +105,69 @@ export default class Packet extends Hashable {
 
     release(): void {
         this.pos = 0;
-        if (this.data.length === 100 && Packet.cacheMinCount < 1000) {
+        if (this.length === 100 && Packet.cacheMinCount < 1000) {
             Packet.cacheMin.addTail(this);
             Packet.cacheMinCount++;
-        } else if (this.data.length === 5000 && Packet.cacheMidCount < 250) {
+        } else if (this.length === 5000 && Packet.cacheMidCount < 250) {
             Packet.cacheMid.addTail(this);
             Packet.cacheMidCount++;
-        } else if (this.data.length === 30000 && Packet.cacheMaxCount < 50) {
+        } else if (this.length === 30000 && Packet.cacheMaxCount < 50) {
             Packet.cacheMax.addTail(this);
             Packet.cacheMaxCount++;
         }
     }
 
     get g1(): number {
-        return this.data[this.pos++];
+        return this.view.getUint8(this.pos++);
     }
 
     // signed
     get g1b(): number {
-        return (this.data[this.pos++] << 24) >> 24;
+        return this.view.getInt8(this.pos++);
     }
 
     get g2(): number {
-        return (this.data[this.pos++] << 8) | this.data[this.pos++];
+        const result: number = this.view.getUint16(this.pos);
+        this.pos += 2;
+        return result;
     }
 
     // signed
     get g2b(): number {
-        let value: number = (this.data[this.pos++] << 8) | this.data[this.pos++];
-        if (value > 0x7fff) {
-            value -= 0x10000;
-        }
-        return value;
+        const result: number = this.view.getInt16(this.pos);
+        this.pos += 2;
+        return result;
     }
 
     get g3(): number {
-        return (this.data[this.pos++] << 16) | (this.data[this.pos++] << 8) | this.data[this.pos++];
+        return (this.g1 << 16) | this.g2;
     }
 
     get g4(): number {
-        return (this.data[this.pos++] << 24) | (this.data[this.pos++] << 16) | (this.data[this.pos++] << 8) | this.data[this.pos++];
+        const result: number = this.view.getInt32(this.pos);
+        this.pos += 4;
+        return result;
     }
 
     get g8(): bigint {
-        return ((BigInt(this.g4) & 0xffffffffn) << 32n) | (BigInt(this.g4) & 0xffffffffn);
+        const result: bigint = this.view.getBigInt64(this.pos);
+        this.pos += 8;
+        return result;
     }
 
     get gsmart(): number {
-        return this.data[this.pos] < 0x80 ? this.g1 - 0x40 : this.g2 - 0xc000;
+        return this.peek < 0x80 ? this.g1 - 0x40 : this.g2 - 0xc000;
     }
 
     // signed
     get gsmarts(): number {
-        return this.data[this.pos] < 0x80 ? this.g1 : this.g2 - 0x8000;
+        return this.peek < 0x80 ? this.g1 : this.g2 - 0x8000;
     }
 
     get gjstr(): string {
         let str: string = '';
-        while (this.data[this.pos] !== 10 && this.pos < this.data.length) {
-            str += String.fromCharCode(this.data[this.pos++]);
+        while (this.peek !== 10 && this.pos < this.length) {
+            str += String.fromCharCode(this.g1);
         }
         this.pos++;
         return str;
@@ -169,68 +175,63 @@ export default class Packet extends Hashable {
 
     gdata(length: number, offset: number, dest: Uint8Array | Int8Array): void {
         for (let i: number = offset; i < offset + length; i++) {
-            dest[i] = this.data[this.pos++];
+            dest[i] = this.g1;
         }
     }
 
     p1isaac(opcode: number): void {
-        this.data[this.pos++] = (opcode + (this.random?.nextInt ?? 0)) & 0xff;
+        this.p1((opcode + (this.random?.nextInt ?? 0)) & 0xff);
     }
 
     p1(value: number): void {
-        this.data[this.pos++] = value;
+        this.view.setUint8(this.pos++, value);
     }
 
     p2(value: number): void {
-        this.data[this.pos++] = value >>> 8;
-        this.data[this.pos++] = value;
+        this.view.setUint16(this.pos, value);
+        this.pos += 2;
     }
 
     ip2(value: number): void {
-        this.data[this.pos++] = value;
-        this.data[this.pos++] = value >>> 8;
+        this.view.setUint16(this.pos, value, true);
+        this.pos += 2;
     }
 
     p3(value: number): void {
-        this.data[this.pos++] = value >>> 16;
-        this.data[this.pos++] = value >>> 8;
-        this.data[this.pos++] = value;
+        this.p1(value >> 16);
+        this.p2(value);
     }
 
     p4(value: number): void {
-        this.data[this.pos++] = value >>> 24;
-        this.data[this.pos++] = value >>> 16;
-        this.data[this.pos++] = value >>> 8;
-        this.data[this.pos++] = value;
+        this.view.setUint32(this.pos, value);
+        this.pos += 4;
     }
 
     ip4(value: number): void {
-        this.data[this.pos++] = value;
-        this.data[this.pos++] = value >>> 8;
-        this.data[this.pos++] = value >>> 16;
-        this.data[this.pos++] = value >>> 24;
+        this.view.setInt32(this.pos, value, true);
+        this.pos += 4;
     }
 
     p8(value: bigint): void {
-        this.p4(Number(value >> 32n));
-        this.p4(Number(value & 0xffffffffn));
+        this.view.setBigInt64(this.pos, value);
+        this.pos += 8;
     }
 
     pjstr(str: string): void {
         for (let i: number = 0; i < str.length; i++) {
-            this.data[this.pos++] = str.charCodeAt(i);
+            this.p1(str.charCodeAt(i));
         }
-        this.data[this.pos++] = 10;
+        this.p1(10);
     }
 
     pdata(src: Uint8Array, length: number, offset: number): void {
         for (let i: number = offset; i < offset + length; i++) {
-            this.data[this.pos++] = src[i];
+            this.p1(src[i]);
         }
     }
 
     psize1(size: number): void {
-        this.data[this.pos - size - 1] = size;
+        this.view.setUint8(this.pos - size - 1, size);
     }
 
     bits(): void {
@@ -248,22 +249,25 @@ export default class Packet extends Hashable {
         this.bitPos += n;
 
         for (; n > remaining; remaining = 8) {
-            value += (this.data[bytePos++] & Packet.bitmask[remaining]) << (n - remaining);
+            value += (this.view.getUint8(bytePos++) & Packet.bitmask[remaining]) << (n - remaining);
             n -= remaining;
         }
 
         if (n === remaining) {
-            value += this.data[bytePos] & Packet.bitmask[remaining];
+            value += this.view.getUint8(bytePos) & Packet.bitmask[remaining];
         } else {
-            value += (this.data[bytePos] >>> (remaining - n)) & Packet.bitmask[n];
+            value += (this.view.getUint8(bytePos) >>> (remaining - n)) & Packet.bitmask[n];
         }
 
         return value;
     }
 
     rsaenc(mod: bigint, exp: bigint): void {
-        const temp: Uint8Array = new Uint8Array(this.pos);
-        arraycopy(this.data, 0, temp, 0, this.pos);
+        const length: number = this.pos;
+        this.pos = 0;
+
+        const temp: Uint8Array = new Uint8Array(length);
+        this.gdata(length, 0, temp);
 
         const bigRaw: bigint = bytesToBigInt(temp);
         const bigEnc: bigint = bigIntModPow(bigRaw, exp, mod);
@@ -272,5 +276,9 @@ export default class Packet extends Hashable {
         this.pos = 0;
         this.p1(rawEnc.length);
         this.pdata(rawEnc, rawEnc.length, 0);
+    }
+
+    private get peek(): number {
+        return this.view.getUint8(this.pos);
     }
 }

src/js/jagex2/config/ComType.ts
@@ -109,7 +109,7 @@ export default class ComType {
 
         dat.pos += 2; // const count = dat.g2;
 
-        while (dat.pos < dat.data.length) {
+        while (dat.pos < dat.length) {
             let id: number = dat.g2;
             if (id === 65535) {
                 layer = dat.g2;

src/js/jagex2/graphics/Pix24.ts
@@ -73,7 +73,7 @@ export default class Pix24 extends Hashable {
             index.pos += 1;
         }
 
-        if (dat.pos > dat.data.length || index.pos > index.data.length) {
+        if (dat.pos > dat.length || index.pos > index.length) {
             throw new Error();
         }
 

src/js/jagex2/graphics/Pix8.ts
@@ -53,7 +53,7 @@ export default class Pix8 extends Hashable {
             index.pos += 1;
         }
 
-        if (dat.pos > dat.data.length || index.pos > index.data.length) {
+        if (dat.pos > dat.length || index.pos > index.length) {
             throw new Error();
         }
 

commit e72271a65c27121932904ef383b0ce68b26d2a22

fix cache viewer after dataview change
broke in https://github.com/2004scape/Client2/commit/c2abe2b40bc32c005a80999a8d506f20b702a835

src/js/jagex2/io/Packet.ts
@@ -65,7 +65,7 @@ export default class Packet extends Hashable {
         } else {
             this.data = src;
         }
-        this.view = new DataView(this.data.buffer);
+        this.view = new DataView(this.data.buffer, this.data.byteOffset, this.data.byteLength);
     }
 
     get length(): number {